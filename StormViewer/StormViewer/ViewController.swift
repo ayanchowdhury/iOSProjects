a4ba188b6e35ce8b293dd7c853d41b72d5a97512
Ayan Chowdhury <ayanchowdhury@berkeley.edu>
2017-10-17 13:02:35 -0700

    Started StormViewer project

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by Ayan  Chowdhury on 10/17/17.
+//  Copyright © 2017 Ayan  Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
