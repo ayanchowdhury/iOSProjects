a8fc90e82daf457f1785d808bc43661ba051b3e0
Ayan Chowdhury <ayanchowdhury@berkeley.edu>
2018-05-13 00:45:23 -0700

    Starting cryptocurrency tracker project

--- FoodTracker/FoodTracker/ViewController.swift
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -24,9 +24,6 @@ class ViewController: UIViewController {
     }
 
     //MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Default Text"
-        
-    }
+    
 }
 
